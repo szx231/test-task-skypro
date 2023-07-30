44619042d76d4be6dd5b06a75f79867625851e40
aqa <82704685+szx231@users.noreply.github.com>
2023-07-31 00:19:10 +0300

    Update constant.ts

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -1,5 +1,5 @@
 export const API_GET_USERS = 'https://api.github.com/search/users?q=';
 export const DEFAULT_USERS_COUNT = 12;
 export const DEFAULT_REPOS_COUNT_PROFILE = 4;
-export const TOKEN_GITHUB = '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';
+export const TOKEN_GITHUB = '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';
 export const API_GET_PROFILE = 'https://api.github.com/users';
